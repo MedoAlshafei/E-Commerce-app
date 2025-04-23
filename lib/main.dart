de8385743af4fdda4399c3a208a168d8ed912d7d
MedoAlshafei <elshafey009@gmail.com>
2025-04-23 20:59:24 +0200

    Add route for UpdateProductPage in StoreApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop/screens/home_page.dart';
+import 'package:shop/screens/update_product_page.dart';
 
 void main() {
   runApp(const StoreApp());
@@ -13,7 +14,10 @@ class StoreApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: HomePage.id,
-      routes: {HomePage.id: (context) => HomePage()},
+      routes: {
+        HomePage.id: (context) => HomePage(),
+        UpdateProductPage.id: (context) => UpdateProductPage(),
+      },
     );
   }
 }
