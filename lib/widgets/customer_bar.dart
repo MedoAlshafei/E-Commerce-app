5c872a28a41892faf95b16a9ee3e2ad717c5a6be
MedoAlshafei <elshafey009@gmail.com>
2025-08-24 14:12:14 +0300

    Add CustomerBar widget and profile image functionality; include assets directory in pubspec.yaml

---
+++ lib/widgets/customer_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CustomerBar extends StatelessWidget {
+  const CustomerBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    final width = size.width;
+    final height = size.height;
+
+    return SizedBox(
+      height: 46,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [_welcomeCustomer(), profileImage()],
+      ),
+    );
+  }
+
+  Widget profileImage() {
+    const String profileImage = 'assets/boy.png';
+    return CircleAvatar(
+      child: Image.asset(profileImage, fit: BoxFit.cover, height: 32),
+    );
+  }
+
+  Widget _welcomeCustomer() {
+    const String helloWelcome = 'Hello, Welcome 👋';
+    const String userName = 'Albert Stevano';
+
+    return Column(
+      children: [
+        Text(
+          helloWelcome,
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+        ),
+        Text(
+          userName,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+        ),
+      ],
+    );
+  }
+}
