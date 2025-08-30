2df42c151bf37ebaec7e58392a003e04883bc928
MedoAlshafei <elshafey009@gmail.com>
2025-08-30 05:02:35 +0300

    Update ProductPage to display product image and require product model as a parameter

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/product_model.dart';
 
 class ProductPage extends StatelessWidget {
-  const ProductPage({super.key});
+  const ProductPage({super.key, required this.product});
 
   static const String id = 'productPage';
 
+  final ProductModel product;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: const Center(child: Text('Product details go here')),
+        child: Container(
+          alignment: Alignment.center,
+          child: Image.network(product.image),
+        ),
       ),
     );
   }
